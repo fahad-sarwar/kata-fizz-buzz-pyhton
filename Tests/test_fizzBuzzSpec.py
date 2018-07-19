e3714ceaebe3cee8f3a906890e2fd5c04d580d8f
Fahad Sarwar <fahad.sarwar@zeninternet.co.uk>
2018-07-19 18:24:54 +0100

    added support for additional rules

--- Tests/test_fizzBuzzSpec.py
+++ Tests/test_fizzBuzzSpec.py
@@ -7,28 +7,34 @@ import pytest
     (3, "fizz"),
     (4, "4"),
     (5, "buzz"),
+    (7, "pop"),
     (9, "fizz"),
     (15, "fizzbuzz"),
     (20, "buzz"),
-    (45, "fizzbuzz")
+    (21, "fizzpop"),
+    (28, "pop"),
+    (35, "buzzpop"),
+    (45, "fizzbuzz"),
+    (63, "fizzpop"),
+    (70, "buzzpop"),
+    (105, "fizzbuzzpop")
 ])
 def test_fizzbuzz_game(number, result):
     assert result == FizzBuzzGame().Speak(number)
 
 
 class FizzBuzzGame():
+    def __init__(self):
+        self._lookup = {3: "fizz", 5: "buzz", 7: "pop"}
+
     def Speak(self, number):
-        if(self.IsFizz(number) and self.IsBuzz(number)):
-            return "fizzbuzz"
+        result = ""
 
-        if (self.IsFizz(number)):
-            return "fizz"
+        for key in self._lookup:
+            if(number % key == 0):
+                result = result + self._lookup[key]
 
-        if (self.IsBuzz(number)):
-            return "buzz"
+        if (result != ""):
+            return result
 
         return str(number)
-
-    def IsFizz(self, number): return number % 3 == 0
-
-    def IsBuzz(self, number): return number % 5 == 0
